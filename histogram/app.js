8a851b2855e10c6531dbaadc98b8c1c4473f2c29
nicktun <nikolaevsky.igor.a@gmail.com>
2024-06-11 19:00:31 +0300

    code reworking

--- histogram/app.js
+++ histogram/app.js
@@ -1,11 +1,15 @@
 const frame = document.getElementById("frame");
+const columnWidth = 56;
+const columnMaxHeight = 320;
+const columnGap = 28;
 document.getElementById("input").addEventListener("input", (e) => {
     refresh(e.target.value);
 })
 
 function refresh(data) {
-    const values = data.split(" ").filter(e => Number(e));
-    const coof = Math.max(...values) / 320;
+    const values = data.split(" ").filter(e => Number(e) || e == "0");
+    console.log(values)
+    const coof = Math.max(...values) / columnMaxHeight;
     if(data) {
         values.forEach((value, index) => {
             const elem = frame.children[index];
@@ -16,28 +20,27 @@ function refresh(data) {
                 const column = document.createElement('div');
                 const wrapper = document.createElement('div');
                 wrapper.className = "flex flex-col justify-end"
-                column.className = "w-0 h-0 mx-auto bg-gradient-to-b from-[#43C7FF] to-[#003E9B] rounded-2xl transition-all duration-500";
-                column.style = "box-shadow: 0px 0px 21.5px 0px rgba(36, 135, 209, 0.30);"; // тут точно смысла нет переводить в css, все стили уникальны для элемента;
-                wrapper.appendChild(column); // а bg-gradient-to-b from-[#43C7FF] to-[#003E9B] тут к слову как один класс посути
-                frame.appendChild(wrapper); // p.s. Я все же считаю что таилвинд это не случай с реакт разработчиком.
-                requestAnimationFrame(() => { // Я начинал с чистого css и если бы я его не знал, то и таилвиндом бы ничего не сделал; А таилвинд куда быстрее ванил css'a.
+                column.className = `w-0 h-0 min-h-[10px] mx-auto bg-gradient-to-b from-[#43C7FF]
+                to-[#003E9B] rounded-2xl transition-all duration-500 shadow-[0px_0px_21.5px_0px_rgba(36,135,209,0.30)]`;
+                wrapper.appendChild(column);
+                frame.appendChild(wrapper);
+                requestAnimationFrame(() => {
                     column.style.height = desiredHeight + "px";
-                    column.style.width = "56px";
+                    column.style.width = columnWidth + "px";
                 })
             }
         });
     }
 
-    for(i = frame.children.length - 1; i >= values.length; i--) {
-        const locali = i;
-        column = frame.children[locali].children[0];
+    for(let i = frame.children.length - 1; i >= values.length; i--) {
+        column = frame.children[i].children[0];
         if(!column.classList.contains('deleted')){
             column.classList.add('deleted');
-            setTimeout(() => {  // Везде искал, нашел как сделать изчезновение после окончания анимации только так;
-                frame.removeChild(frame.children[locali]); // event transitionend/animationend не получилось юзать, т.к срабатывает на ненужных транзишинах
-            }, 300)
+            column.addEventListener("transitionend", (e) => {
+                frame.removeChild(e.target.parentElement);
+            }, { once: true });
         }
     }
 
-    frame.style.width = Math.abs(values.length * 56 + (values.length - 1) * 28) + "px";
+    frame.style.width = Math.abs(values.length * columnWidth + (values.length - 1) * columnGap) + "px";
 }
